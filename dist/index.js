469bdecb7af0e771a22a2d474adeb11e4bd9c667
superdev7788 <86911619+superdev7788@users.noreply.github.com>
2025-09-18 17:13:32 +0530

    Compatible version with verbatimModuleSyntax set true

---
+++ dist/index.js
@@ -0,0 +1,39 @@
+import express, {} from "express";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+const app = express();
+app.use(express.json());
+// Get all users
+app.get("/users", async (req, res) => {
+    const users = await prisma.user.findMany();
+    res.json(users);
+});
+// Get user by id
+app.get("/users/:id", async (req, res) => {
+    const user = await prisma.user.findUnique({
+        where: { id: Number(req.params.id) },
+    });
+    res.json(user);
+});
+// Create user
+app.post("/users", async (req, res) => {
+    const user = await prisma.user.create({ data: req.body });
+    res.json(user);
+});
+// Update user
+app.put("/users/:id", async (req, res) => {
+    const user = await prisma.user.update({
+        where: { id: Number(req.params.id) },
+        data: req.body,
+    });
+    res.json(user);
+});
+// Delete user
+app.delete("/users/:id", async (req, res) => {
+    const user = await prisma.user.delete({
+        where: { id: Number(req.params.id) },
+    });
+    res.json(user);
+});
+app.listen(3000, () => console.log("REST API server ready at: http://localhost:3000"));
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { type Request, type Response } from "express";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
